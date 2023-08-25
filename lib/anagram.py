8cc6f459d097bf173ba1f82be388506929465d65
Hiroki Kato <hirokikato1@gmail.com>
2023-08-25 12:12:01 -0700

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+import ipdb
+
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, anagram_list):    
+        original_word = sorted(self.word)
+        matches = []
+        for possible_match in anagram_list:
+            if original_word == sorted(possible_match):
+                matches.append(possible_match)
+        
+        return matches
+
+    
+
+
